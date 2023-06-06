5136bd3c67e5ce779c74d082d6489fdde95d400d
liunanno <129089833+liunanno@users.noreply.github.com>
2023-06-06 18:45:20 +0200

    Add files via upload

---
+++ ft_printf/ft_printf.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nliu <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/06 17:42:19 by nliu              #+#    #+#             */
+/*   Updated: 2023/06/06 18:40:59 by nliu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void	ft_format(va_list va, char *str, size_t *counter)
+{
+	if (*str == 'c')
+		ft_putchar_pf(va_arg(va, int), counter);
+	else if (*str == 's')
+		ft_putstr_pf(va_arg(va, char *), counter);
+	else if (*str == 'p')
+		ft_putnbr_pf(va_arg(va, void *), counter);
+	else if (*str == 'i' || *str == 'd')
+		ft_putnbr_pf(va_arg(va, int), counter);
+	else if (*str == 'u')
+		ft_putuint_pf(va_arg(va, unsigned int), counter);
+	else if (*str == 'x' || *str == 'X')
+	{
+		if (*str == 'x')
+			ft_puthex_pf(va_arg(va, unsigned int), counter, HEX_LOW_BASE);
+		else
+			ft_puthex_pf(va_arg(va, unsigned int), counter, HEX_UPP_BASE);
+	}
+	else if (*str == '%')
+		ft_putchar_pf(*str, counter);
+}
+
+int	ft_printf(char const *str, ...)
+{
+	va_list	va;
+	size_t	counter;
+
+	if (!str)
+		return (0);
+	counter = 0;
+	va_start(va, str);
+	while (*str)
+	{
+		if (*str == '%')
+		{
+			str++;
+			ft_format(va, (char *)str, &counter);
+		}
+		else
+			ft_putchar_pf(*str, &counter);
+		str++;
+	}
+	va_end(va);
+	return (counter);
+}
