331202e47fa800daca0eb50af8537c9bb10fc80a
liunanno <129089833+liunanno@users.noreply.github.com>
2023-07-01 20:51:22 +0200

    Add files via upload

--- exam02/Level1/repeat_alpha/repeat_alpha2.c
+++ exam02/Level1/repeat_alpha/repeat_alpha2.c
@@ -1,27 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   first_word.c                                       :+:      :+:    :+:   */
+/*   repeat_alpha2.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nliu <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/25 21:27:58 by nliu              #+#    #+#             */
-/*   Updated: 2023/06/28 13:27:36 by nliu             ###   ########.fr       */
+/*   Created: 2023/06/28 22:40:08 by nliu              #+#    #+#             */
+/*   Updated: 2023/06/28 23:06:36 by nliu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <unistd.h>
 
 int	main(int argc, char **argv)
 {
 	int	i;
+	int j;
+	char c;
 
 	i = 0;
-	if (argc ==  2)
+	j = 0;
+	c = argv[1][i];
+	if (argc == 2)
 	{
-		while (argv[1][i] == 32 && argv[1][i] == 9)
+		while (argv[1][i])
+		{
+			if ((argv[1][i] >= 'a' && argv[1][i] <= 'z') || (argv[1][i] >= 'A' && argv[1][i] <= 'Z'))
+			{
+				if (c >= 'a' && c <= 'z')
+					c -= 32;
+				while (j <= (c - 'A'))
+				{
+					write(1, &argv[1][i], 1);
+					j++;
+				}
+			}
+			else
+				write(1, &argv[1][i], 1);
 			i++;
-		while ((argv[1][i] != 32 && argv[1][i] != 9) && argv[1][i])
-			write(1, &argv[1][i], 1);
-			i++;
+		}			
 	}
 	write(1, "\n", 1);
+	return (0);
 }
--- exam02/Level1/repeat_alpha/repeat_alpha5.c
+++ exam02/Level1/repeat_alpha/repeat_alpha5.c
@@ -1,27 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   first_word.c                                       :+:      :+:    :+:   */
+/*   repeat_alpha5.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nliu <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/25 21:27:58 by nliu              #+#    #+#             */
-/*   Updated: 2023/06/28 13:27:36 by nliu             ###   ########.fr       */
+/*   Created: 2023/07/01 16:25:57 by nliu              #+#    #+#             */
+/*   Updated: 2023/07/01 16:31:45 by nliu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main(int argc, char **argv)
-{
-	int	i;
+#include <unistd.h>
 
-	i = 0;
-	if (argc ==  2)
+void	ft_putchar_n(char c, int i)
+{
+	while (i > 0)
 	{
-		while (argv[1][i] == 32 && argv[1][i] == 9)
-			i++;
-		while ((argv[1][i] != 32 && argv[1][i] != 9) && argv[1][i])
-			write(1, &argv[1][i], 1);
-			i++;
+		write(1, &c, 1);
+		--i;
 	}
+}
+
+void	repeat_alpha(char *str)
+{
+	while (*str != '\0')
+	{
+		if (*str >= 'a' && *str <= 'z')
+			ft_putchar_n(*str, *str + 1 - 'a');
+		else if (*str >= 'A' && *str <= 'Z')
+			ft_putchar_n(*str, *str + 1 - 'A');
+		else
+			write(1, str, 1);
+		++str;
+	}
+}
+
+int	main(int ac, char **av)
+{
+	if (ac == 2)
+		repeat_alpha(av[1]);
+
 	write(1, "\n", 1);
+	return (0);
 }
