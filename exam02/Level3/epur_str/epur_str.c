b5486bf62815cf53f64578c520ab78aa32f3fd9b
liunanno <129089833+liunanno@users.noreply.github.com>
2023-09-06 19:04:04 +0200

    Add files via upload

--- exam02/Level3/epur_str/epur_str.c
+++ exam02/Level3/epur_str/epur_str.c
@@ -1,29 +1,23 @@
 #include <unistd.h>
 
-int main(int ac, char const **av)
+int main(int ac, char **av)
 {
     int i;
-    int flg;
 
+    i = 0;
     if (ac == 2)
     {
-        i = 0;
-        while (av[1][i] == ' ' || av[1][i] == '\t')
-            i += 1;
+        while (av[1][i] == ' ' && av[1][i] == '\t')
+            i++;
         while (av[1][i])
         {
-            if (av[1][i] == ' ' || av[1][i] == '\t')
-                flg = 1;
-            if (!(av[1][i] == ' ' || av[1][i] == '\t'))
-            {
-                if (flg)
-                    write(1, " ", 1);
-                flg = 0;
+            while ((av[1][i] == ' ' || av[1][i] == '\t') && \
+            (av[1][i + 1] == ' ' || av[1][i + 1] == '\t'))
+                i++;
+            if (av[1][i + 1])
                 write(1, &av[1][i], 1);
-            }
-            i += 1;
+            i++;
         }
     }
     write(1, "\n", 1);
-    return (0);
 }
