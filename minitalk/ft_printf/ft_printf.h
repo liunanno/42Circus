cab4cb105440c403e6f0007d28debfa3fd7d068e
liunanno <129089833+liunanno@users.noreply.github.com>
2023-10-07 21:10:57 +0200

    Add files via upload

---
+++ minitalk/ft_printf/ft_printf.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nliu <nliu@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/08 13:49:48 by nliu              #+#    #+#             */
+/*   Updated: 2023/10/02 18:46:47 by nliu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# define HEX_UPP_BASE "0123456789ABCDEF"
+# define HEX_LOW_BASE "0123456789abcdef"
+
+int     ft_printf(char const *str, ...);
+
+void    ft_putchar(char c, size_t *counter);
+void    ft_putstr(char *str, size_t *counter);
+void    ft_putnbr(int num, size_t *counter);
+void    ft_putuint(unsigned int num, size_t *counter);
+void    ft_puthex(unsigned int num, size_t *counter, char *base);
+void    ft_putptr(void *ptr, size_t *counter);
+
+char    *ft_aux(unsigned long long n, char *base);
+
+#endif
---
+++ minitalk/minitalk.h
@@ -0,0 +1,5 @@
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "libft/libft.h"
+#include "ft_printf/ft_printf.h"
