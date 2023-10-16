9f0ef7b3fc749f0ace18daf42fe0446117e1f0a1
liunanno <129089833+liunanno@users.noreply.github.com>
2023-10-16 19:03:24 +0200

    Add files via upload

---
+++ minitalk/utils.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nliu <nliu@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/16 18:57:57 by nliu              #+#    #+#             */
+/*   Updated: 2023/10/16 19:01:59 by nliu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+void	ft_putnbr(int nb)
+{
+	long	i;
+
+	i = nb;
+	if (i < 0)
+	{
+		i = -i;
+		ft_putchar('-');
+	}
+	if (i <= 9)
+	{
+		ft_putchar(i + '0');
+	}
+	else
+	{
+		ft_putnbr(i / 10);
+		ft_putnbr(i % 10);
+	}
+}
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	s;
+	int	result;
+
+	i = 0;
+	s = 1;
+	result = 0;
+	while (str[i] == '\t' || str[i] == '\n' || str[i] == '\v'
+		|| str[i] == '\f' || str[i] == '\r' || str[i] == ' ')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			s = -s;
+		i++;
+	}
+	while ((str[i] <= '9' && str[i] >= '0') && str[i] != '\0')
+	{
+		result = result * 10 + str[i] - '0';
+		i++;
+	}
+	return (result * s);
+}
