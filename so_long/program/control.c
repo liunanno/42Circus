8a8a97fa55df88af42ec00df16ed98069ff12efb
liunanno <129089833+liunanno@users.noreply.github.com>
2023-11-01 20:04:34 +0100

    Add files via upload

---
+++ so_long/program/control.c
@@ -0,0 +1,117 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   control.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nliu <nliu@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/01 19:02:12 by nliu              #+#    #+#             */
+/*   Updated: 2023/11/01 19:52:00 by nliu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "solong.h"
+
+void	wall_control(t_game *game)
+{
+	unsigned long	x;
+	int				y;
+
+	y = 0;
+	while (y < (game->size_y / SIZE))
+	{
+		x = 0;
+		while (x < (game->size_x / SIZE))
+		{
+			if ((y == 0 || x == 0) && game->map[y][x] != '1')
+				message(RED"Error\nNo enough walls\n"ENDC, game);
+			else if ((y == (game->size_y / SIZE - 1)
+					|| x == (game->size_x / SIZE - 1))
+				&& game->map[y][x] != '1')
+				message(RED"Error\nNo enough walls\n"ENDC, game);
+			x++;
+		}
+		y++;
+	}
+}
+
+void	map_control(t_game *game)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (game->map[i])
+	{
+		j = 0;
+		while (game->map[i][j])
+		{
+			if (game->map[i][j] == 'E')
+				game->exitcheck++;
+			if (game->map[i][j] == 'N')
+				game->enemycheck++;
+			j++;
+		}
+		i++;
+	}
+	if (game->coincheck == 0)
+		message(RED"No shit to eat!\n"ENDC, game);
+	if (game->enemycheck == 0)
+		message(RED"No hedgehog!\n"ENDC, game);
+	if (game->exitcheck == 0)
+		message("Error\nNo exit!\n", game);
+	if (game->playercheck != 1)
+		message("Error\nOnly one player\n", game);
+}
+
+void	game_control(t_game *game)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (game->map[i])
+	{
+		j = 0;
+		while (game->map[i][j])
+		{
+			if (game->map[i][j] == 'C')
+				game->coincheck++;
+			if (game->map[i][j] == 'P')
+			{
+				game->player.x = j * SIZE;
+				game->player.y = i * SIZE;
+				game->playercheck++;
+			}
+			j++;
+		}
+		i++;
+	}
+	map_control(game);
+	wall_control(game);
+}
+
+int	move_check(t_game *game, int i, int j)
+{
+	if (game->map[i][j] == '1')
+		return (1);
+	else if (game->map[i][j] == 'N')
+		message(RED"You have eaten a hedgehog!\n", game);
+	else if (game->map[i][j] == 'C')
+	{
+		game->player.coin++;
+		game->map[i][j] = '0';
+	}
+	else if (game->map[i][j] == 'E')
+	{
+		if (game->player.coin != game->coincheck)
+		{
+			printf(RED"Did you eat all shits?\n"ENDC);
+			return (1);
+		}
+		message(GREEN"You have eaten all shits\n"ENDC, game);
+		return (0);
+	}
+	return (0);
+}
---
+++ so_long/program/get_next_line.c
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nliu <nliu@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/01 19:05:00 by nliu              #+#    #+#             */
+/*   Updated: 2023/11/01 19:52:44 by nliu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "solong.h"
+
+int	ft_line_length(int fd)
+{
+	char	buffer[0];
+	int		length;
+	int		bytes;
+
+	buffer[0] = '\0';
+	bytes = 1;
+	length = 0;
+	while (bytes == 1)
+	{
+		bytes = read(fd, buffer, 1);
+		if (buffer[0] != '\n')
+			length++;
+		else
+			break ;
+	}
+	return (length);
+}
+
+int	ft_count_lines(int fd)
+{
+	int		linecount;
+	char	buffer[0];
+	int		bytes;
+
+	buffer[0] = '\0';
+	linecount = 1;
+	while (1)
+	{
+		bytes = read(fd, buffer, 1);
+		if (bytes < 1)
+			break ;
+		if (buffer[0] == '\n')
+			linecount++;
+	}
+	return (linecount);
+}
+
+void	*myfree(void *str)
+{
+	free(str);
+	return (NULL);
+}
+
+char	*get_next_line(int fd)
+{
+	char	buffer;
+	char	*line;
+	int		rd_byte;
+	int		i;
+
+	rd_byte = 1;
+	i = 0;
+	line = (char *)malloc(sizeof(char) * 9999);
+	buffer = 0;
+	if (fd < 0)
+		return (NULL);
+	while (rd_byte > 0)
+	{
+		rd_byte = read(fd, &buffer, 1);
+		if (rd_byte <= 0)
+			break ;
+		line[i++] = buffer;
+		if (buffer == '\n')
+			break ;
+	}
+	line[i] = '\0';
+	if (!*line)
+		myfree(line);
+	return (line);
+}
